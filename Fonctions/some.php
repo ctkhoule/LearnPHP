66ae6b46b085e80299fbbd2f543c40f9b433d713
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-04-18 01:53:18 +0000

    Fonction some

---
+++ Fonctions/some.php
@@ -0,0 +1,9 @@
+<?php
+// Fonction some
+    function some($var1, $var2){
+        $sum = $var1 + $var2;
+        echo "La some de $var1 et $var2 est : " . $sum;
+    }
+// Appel
+    some(44, 12);
+?>
